b896ff1b446c2765c61c13c46c4f05a389881844
Hendridaffa <hendridaffa23@gmail.com>
2021-03-30 15:18:56 +0700

    Menambahkan Data Barang

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -18,11 +18,15 @@ class BarangSeeder extends Seeder
     {
         DB::table('barangs')->insert([
             'kode_barang'=>['PRD001', 'PRD002', 'PRD003', 'PRD004', 'PRD005', 'PRD006', 'PRD007', 'PRD008', 'PRD009', 'PRD010',
-                            'PRD011', 'PRD012', 'PRD013', 'PRD014', 'PRD015'],
-            'nama_barang'=>['Indomie', 'Pocari Sweet', 'Silverqueen'],
-            'kategori_barang'=>['Makanan', 'Minuman', 'Snack'],
-            'harga'=>['3000', '6000', '12500'],
-            'qty'=>['100', '50', '45']
+                            'PRD011', 'PRD012', 'PRD013', 'PRD014', 'PRD015', 'PRD016', 'PRD017', 'PRD018', 'PRD019', 'PRD020'],
+            'nama_barang'=>['Indomie', 'Pocari Sweet', 'Silverqueen', 'Stela Apel', 'Sari Roti', 'Frutie', 'Kusuka', 'Axe', 
+                            'Roti Sisir', 'Teh Pucuk', 'KitKat', 'Glade Ocean', 'Roti Tawar', 'Aqua', 'Astor', 'Bayfresh Everywhere'
+                            , 'Roti Gandum', 'Milo', 'Tango', 'Stella Matic'],
+            'kategori_barang'=>['Makanan', 'Minuman', 'Snack', 'Pewangi', 'Makanan', 'Minuman', 'Snack', 'Pewangi', 'Makanan', 'Minuman', 'Snack', 'Pewangi',
+                                'Makanan', 'Minuman', 'Snack', 'Pewangi', 'Makanan', 'Minuman', 'Snack', 'Pewangi'],
+            'harga'=>['3000', '6000', '12500', '10000', '8500', '3000', '5000', '11000', '25000', '7500', '3500', '15000', '20000', '18000'
+                        , '4000', '9000', '30000', '14500', '6000', '10500'],
+            'qty'=>['100', '50', '45', '40', '30', '60', '35', '20', '75', '80', '200', '10', '13', '74', '33', '18', '47', '55', '95', '25']
         ]);
     }
 }
